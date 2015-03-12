143c65a2e586948d51d5d29acb2d207b5dea018e
wxc-thinkpad <wangxc87@163.com>
2015-03-12 20:15:52 +0800

    添加5/buttons_tsklet

---
+++ char_driver/5/.tmp_versions/buttons_tsklet.mod
@@ -0,0 +1,2 @@
+/home/wxc/dsp/ex_part/mini/example/ldd_edu/char_driver/5/buttons_tsklet.ko
+/home/wxc/dsp/ex_part/mini/example/ldd_edu/char_driver/5/buttons_tsklet.o
--- char_driver/5/buttons_tsklet.c
+++ char_driver/5/buttons_tsklet.c
@@ -2,19 +2,9 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/init.h>
-#include <linux/delay.h>
-#include <linux/poll.h>
-#include <linux/irq.h>
-#include <asm/irq.h>
 #include <linux/interrupt.h>
-#include <asm/uaccess.h>
 #include <mach/regs-gpio.h>
-#include <mach/hardware.h>
-#include <linux/platform_device.h>
-#include <linux/cdev.h>
 #include <linux/miscdevice.h>
-#include <linux/sched.h>
-#include <linux/gpio.h>
 
 #define DEVICE_NAME     "buttons"
 
@@ -36,30 +26,23 @@ static struct button_irq_desc button_irqs [] = {
 };
 static volatile char key_values [] = {'0', '0', '0', '0', '0', '0'};
 
-static DECLARE_WAIT_QUEUE_HEAD(button_waitq);
-
-static volatile int ev_press = 0;
-
+//定义实际的底半部调用函数
+void buttons_do_tasklet(unsigned long arg);
+DECLARE_TASKLET(buttons_tasklet, buttons_do_tasklet, 0);
 
 static irqreturn_t buttons_interrupt(int irq, void *dev_id)
 {
     struct button_irq_desc *button_irqs = (struct button_irq_desc *)dev_id;
-    int down;
 
-    // udelay(0);
-    down = !s3c2410_gpio_getpin(button_irqs->pin);
-
-    if (down != (key_values[button_irqs->number] & 1)) { // Changed
-
-        key_values[button_irqs->number] = '0' + down;
-	
-        ev_press = 1;
-        wake_up_interruptible(&button_waitq);
-    }
+    tasklet_schedule(&buttons_tasklet);//调度底半部函数
     
     return IRQ_RETVAL(IRQ_HANDLED);
 }
 
+void buttons_do_tasklet(unsigned long arg)
+{
+    printk("%s enter..\n",__func__);
+}
 
 static int s3c24xx_buttons_open(struct inode *inode, struct file *file)
 {
@@ -70,7 +53,7 @@ static int s3c24xx_buttons_open(struct inode *inode, struct file *file)
         if (button_irqs[i].irq < 0) {
             continue;
         }
-        err = request_irq(button_irqs[i].irq, buttons_interrupt, IRQ_TYPE_EDGE_BOTH, 
+        err = request_irq(button_irqs[i].irq, buttons_interrupt, IRQF_TRIGGER_FALLING,  //注册中断服务程序
                           button_irqs[i].name, (void *)&button_irqs[i]);
         if (err)
             break;
@@ -82,13 +65,12 @@ static int s3c24xx_buttons_open(struct inode *inode, struct file *file)
             if (button_irqs[i].irq < 0) {
                 continue;
             }
-            disable_irq(button_irqs[i].irq);
-            free_irq(button_irqs[i].irq, (void *)&button_irqs[i]);
+            /* disable_irq(button_irqs[i].irq); */
+            free_irq(button_irqs[i].irq, (void *)&button_irqs[i]); //释放注册的中断
         }
         return -EBUSY;
     }
 
-    ev_press = 1;
     
     return 0;
 }
@@ -108,41 +90,10 @@ static int s3c24xx_buttons_close(struct inode *inode, struct file *file)
     return 0;
 }
 
-
-static int s3c24xx_buttons_read(struct file *filp, char __user *buff, size_t count, loff_t *offp)
-{
-    unsigned long err;
-
-    if (!ev_press) {
-        if (filp->f_flags & O_NONBLOCK)
-            return -EAGAIN;
-        else
-            wait_event_interruptible(button_waitq, ev_press);
-    }
-    
-    ev_press = 0;
-
-    err = copy_to_user(buff, (const void *)key_values, min(sizeof(key_values), count));
-
-    return err ? -EFAULT : min(sizeof(key_values), count);
-}
-
-static unsigned int s3c24xx_buttons_poll( struct file *file, struct poll_table_struct *wait)
-{
-    unsigned int mask = 0;
-    poll_wait(file, &button_waitq, wait);
-    if (ev_press)
-        mask |= POLLIN | POLLRDNORM;
-    return mask;
-}
-
-
 static struct file_operations dev_fops = {
     .owner   =   THIS_MODULE,
     .open    =   s3c24xx_buttons_open,
     .release =   s3c24xx_buttons_close, 
-    .read    =   s3c24xx_buttons_read,
-    .poll    =   s3c24xx_buttons_poll,
 };
 
 static struct miscdevice misc = {
